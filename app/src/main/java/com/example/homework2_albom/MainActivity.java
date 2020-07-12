e720f5c784ff7b2d013ebe5cbd67d77d6ec14b66
Elena Valieva <valieva-1991@mail.ru>
2020-07-12 12:28:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework2_albom/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.homework2_albom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonBack = findViewById(R.id.buttonBack);
+        Button buttonForward = findViewById(R.id.buttonForward);
+        final TextView textView = findViewById(R.id.textrandom);
+        
+        buttonForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int value = 1 + (int) (Math.random() * 100);
+                textView.setText("http://myfile.org/" + value);
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
